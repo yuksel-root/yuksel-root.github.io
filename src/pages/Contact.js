211be39ea1909342cfabe827d2b70c7df8474d8d
Yüksel Can Şimşek <68549407+yuksi-root@users.noreply.github.com>
2021-05-29 21:30:17 +0300

    Add files via upload

---
+++ src/pages/Contact.js
@@ -0,0 +1,46 @@
+import React from "react";
+import '../styles/Contact.css';
+import '../App.css';
+
+function Contacts() {
+  return (
+    <section className="contact section" id="contact">
+      <span className="section-subtitle">Contact Informations</span>
+      <h2 className="section-title">Contact Me</h2>
+
+      <div className="contact__container bd-grid">
+        <div className="contact__info">
+          <h3 className="contact__subtitle">
+            <a href="# " className="home__social-link">
+              <i className="bx bx-envelope"></i>
+            </a>
+            E-mail
+          </h3>
+          <span className="contact__text">yukselcansimsek@gmail.com</span>
+        </div>
+
+        <div className="contact__info">
+          <h3 className="contact__subtitle">
+            <a href="# " className="home__social-link">
+              <i className="bx bxl-linkedin"></i>
+            </a>
+            Linkedin
+          </h3>
+          <span className="contact__text">linkedin.com/in/yuksi-root/</span>
+        </div>
+
+        <div className="contact__info">
+          <h3 className="contact__subtitle">
+            <a href="# " className="home__social-link">
+              <i className="bx bx-current-location"></i>
+            </a>
+            Location
+          </h3>
+          <span className="contact__text">Turkey - Bursa</span>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Contacts;
---
+++ src/pages/Header.js
@@ -0,0 +1,90 @@
+import React, { useState } from "react";
+import "../styles/Header.css";
+import "../App.css";
+
+function Header() {
+  const [isNavOpen, setIsNavOpen] = useState(false);
+  const [isActive, setIsActive] = useState("0");
+  
+  return (
+    <header className={isNavOpen ? "nav-open" : ""}>
+      <div className="l-header">
+        <nav className="nav bd-grid">
+          <button
+            className="nav__toggle"
+            onClick={() => setIsNavOpen(!isNavOpen)}
+          >
+            <i className="bx bx-menu"></i>
+          </button>
+
+          <div onClick={() => setIsNavOpen(!isNavOpen)}>
+            <a href="# " className="nav__logo" onClick={() => setIsActive("0")}>
+              Yüksel
+            </a>
+          </div>
+
+          <div className="nav__menu">
+            <button
+              className="nav__close"
+              onClick={() => setIsNavOpen(!isNavOpen)}
+            >
+              <i className="bx bx-x"></i>
+            </button>
+
+            <ul className="nav__list">
+              <li className="nav__item" onClick={() => setIsActive("0")}>
+                <a
+                  href="#home"
+                  className={
+                    isActive === "0" ? "nav__link active" : "nav__link"
+                  }
+                  onClick={() => setIsNavOpen(!isNavOpen)}
+                >
+                  Home
+                </a>
+              </li>
+
+              <li className="nav__item" onClick={() => setIsActive("1")}>
+                <a
+                  href="#about"
+                  className={
+                    isActive === "1" ? "nav__link active" : "nav__link"
+                  }
+                  onClick={() => setIsNavOpen(!isNavOpen)}
+                >
+                  About
+                </a>
+              </li>
+
+              <li className="nav__item" onClick={() => setIsActive("2")}>
+                <a
+                  href="#works"
+                  className={
+                    isActive === "2" ? "nav__link active" : "nav__link"
+                  }
+                  onClick={() => setIsNavOpen(!isNavOpen)}
+                >
+                  Projects
+                </a>
+              </li>
+
+              <li className="nav__item" onClick={() => setIsActive("3")}>
+                <a
+                  href="#contact"
+                  className={
+                    isActive === "3" ? "nav__link active" : "nav__link"
+                  }
+                  onClick={() => setIsNavOpen(!isNavOpen)}
+                >
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
