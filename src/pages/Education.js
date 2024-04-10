a30ef0e1ba98abf3671b89f51c667626f35c4a07
unknown <yukselcansimsek@gmail.com>
2024-04-10 18:35:51 +0300

    updated main branch

--- src/pages/Education.js
+++ src/pages/Education.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import '../styles/Education.css';
 import '../App.css';
@@ -34,40 +33,3 @@ function Education() {
 }
 
 export default Education;
-=======
-import React from "react";
-import '../styles/Education.css';
-import '../App.css';
-
-function Education() {
-  return (
-    <section className="education section">
-      <span className="section-subtitle">Qualification</span>
-      <h2 className="section-title">My Education</h2>
-
-      <div className="education__container bd-grid">
-        <div className="education__content">
-          <div>
-            <h3 className="education__year">2017 - 2022</h3>
-            <span className="education__university">
-              Karadeniz Technical University
-            </span>
-          </div>
-
-          <div className="education__time">
-            <span className="education__rounder"></span>
-            <span className="education__line"></span>
-          </div>
-
-          <div>
-            <h3 className="education__race">Statistics and Computer Sciences</h3>
-            <span className="education__specialty">Bachelor Degree</span>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
-}
-
-export default Education;
->>>>>>> 1bcfb92849dad7ad143d7b07a2c276a874934989
